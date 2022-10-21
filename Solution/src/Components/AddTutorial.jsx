ea5de27475f46aa6f850ae955068e2b03cea3e24
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-10-21 03:02:36 +0200

    Add Tutorial

--- Solution/src/Components/AddTutorial.jsx
+++ Solution/src/Components/AddTutorial.jsx
@@ -1,8 +1,32 @@
-const AddTutorial = () => {
+import axios from "axios";
+import { useState } from "react";
+
+const AddTutorial = ({getTutorials}) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTutorial = { title, description };
+    postTutorial(newTutorial);
+    setTitle("");
+    setDescription("");
+  };
+  
+  const postTutorial = async (newTutorial) => {
+    const url = `https://tutorials-api-cw.herokuapp.com/api/tutorials`;
+    try {
+      await axios.post(url, newTutorial);
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
+
   return (
-    <div className="container">
-        <h1 className="display-6 text-danger text-center mt-3">Tutorial List</h1>
-      <form className="text-center">
+    <div className="addTutorial my-5">
+      <h1 className="display-6 text-center text-danger">Add Your Tutorial</h1>
+      <form onSubmit={handleSubmit} className="text-center mx-5">
         <div className="mb-3">
           <label htmlFor="title" className="form-label">
             Title
@@ -11,6 +35,9 @@ const AddTutorial = () => {
             type="text"
             className="form-control"
             id="title"
+            placeholder="Enter your title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="mb-3">
@@ -20,7 +47,10 @@ const AddTutorial = () => {
           <input
             type="text"
             className="form-control"
-            id="descriptiuon"
+            id="description"
+            placeholder="Enter Your Description"
+            value={description}
+            onChange={(e)=> setDescription(e.target.value)}
           />
         </div>
         <button type="submit" className="btn btn-danger">
