30754b4825bada6ad7e299e79375f2776be44648
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-10-21 03:03:50 +0200

    Tutorial List

--- Solution/src/Components/TutorialList.jsx
+++ Solution/src/Components/TutorialList.jsx
@@ -1,32 +1,45 @@
 import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
+import axios from "axios";
+import EditTutorial from "./EditTutorial";
+import { useState } from "react";
 
-const TutorialList = () => {
-  const tutorialExample = [
-    {
-      id: 1,
-      title: "merhaba",
-      description: "deneme",
-    },
-    {
-      id: 1,
-      title: "merhaba",
-      description: "deneme",
-    },
-  ];
+const TutorialList = ({tutorials, getTutorials}) => {
+  const [editItem, setEditItem] = useState("");
+
+  const editTutorial = async (id, title, description) => {
+    const url = `https://tutorials-api-cw.herokuapp.com/api/tutorials`
+    const edited = tutorials
+      .filter((tutor) => tutor.id === id)
+      .map(() => ({ title: title, description: description }));
+
+    await axios.put(`${url}/${id}`, edited[0]);
+    getTutorials();
+  };
+
+  const deleteTutorial = async (id) => {
+    const url = `https://tutorials-api-cw.herokuapp.com/api/tutorials`
+    try {
+      await axios.delete(`${url}/${id}`)
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials()
+  } 
+
   return (
     <div className="container mt-4">
       <table className="table table-striped">
         <thead>
           <tr>
-            <th>#id</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th className="text-center">Edit</th>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">Edit</th>
           </tr>
         </thead>
         <tbody>
-          {tutorialExample.map((item) => {
+          {tutorials?.map((item) => {
             const { id, title, description } = item;
             return (
               <tr key={id}>
@@ -34,18 +47,27 @@ const TutorialList = () => {
                 <td>{title}</td>
                 <td>{description}</td>
                 <td className="text-center text-nowrap">
-                  <FaEdit type="button" className="text-warning" size={20} />
-                  <AiFillDelete
-                    type="button"
-                    className="text-danger"
+                  <FaEdit
                     size={20}
-                  />
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                    className="me-2 text-warning"
+                    onClick={() => setEditItem(item)}
+                    />
+                  <AiFillDelete 
+                    size={22}
+                    type='button'
+                    className="text-danger"
+                    onClick={()=> deleteTutorial(id)}
+                    />
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      <EditTutorial editItem={editItem} editTutorial={editTutorial}/>
     </div>
   );
 };
