d9a3cc097bebe1d901bd632eeb5b0053a4cceff6
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-10-21 03:04:20 +0200

    Home

--- Solution/src/Pages/Home.jsx
+++ Solution/src/Pages/Home.jsx
@@ -1,15 +1,33 @@
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 import AddTutorial from "../Components/AddTutorial";
 import TutorialList from "../Components/TutorialList";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
   const url = `https://tutorials-api-cw.herokuapp.com/api/tutorials`;
-  
+
+  const getTutorials = async () => {
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+      setTutorials(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTutorials();
+  }, []);
 
   return (
-    <div className="container">
-      <AddTutorial />
-      <TutorialList />
-    </div>
+    <>
+      <AddTutorial getTutorials={getTutorials}/>
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials}/>
+    </>
   );
 };
 
